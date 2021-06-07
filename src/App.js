63342b5bb143c5af6d7f6d3e5036c2d5e6fe4c4f
outcasted9044 <realweirdo904@gmail.com>
2021-06-08 01:35:49 +0300

    minor bug fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Signin from './components/Signin';
 import ChatRoom from './components/ChatRoom';
 
 firebase.initializeApp({
-	apiKey: process.env.REACT_APP_apikey,
+	apiKey: process.env.REACT_APP_apiKey,
 	authDomain: process.env.REACT_APP_authDomain,
 	projectId: process.env.REACT_APP_projectId,
 	storageBucket: process.env.REACT_APP_storageBucket,
